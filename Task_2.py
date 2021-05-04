aaa433b0749df7f20f21a5127ad9a5f69ceef9ac
iraSemchuk <irynaa.semchuk@gmail.com>
2021-05-04 07:22:10 -0700

    Adding Task_2

---
+++ Task_2.py
@@ -0,0 +1,52 @@
+'''
+2. Create two classes: Person, Cell Phone, one for composition, another one for aggregation.
+  a.
+      class Person:
+          """
+          Make the class with composition.
+          """
+      class Arm:
+          """
+          Make the class with composition.
+          """
+'''
+class Person:
+    def __init__(self):
+        arm1 = Arm('System command 1')
+        arm2 = Arm('System command 2')
+        self.arms = [arm1, arm2]
+
+class Arm:
+    def __init__(self, command):
+        self.command = command
+
+print('______Composition______')
+person = Person()
+for arm in person.arms:
+    print(arm.command)
+
+'''
+b.
+      class CellPhone:
+          """
+          Make the class with aggregation
+          """
+      class Screen:
+          """
+          Make the class with aggregation
+          """
+'''
+class CellPhone:
+    def __init__(self, screen):
+        self.screen = screen
+
+
+class Screen:
+    def __init__(self, size):
+        self.size = size
+
+
+print('_____Aggregation______')
+scr = Screen('6.3')
+ph = CellPhone(scr)
+print(ph.screen.size)
