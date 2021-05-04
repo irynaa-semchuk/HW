6cd77be318a02cedc0b074a4326a4232e40955d6
iraSemchuk <irynaa.semchuk@gmail.com>
2021-05-04 07:22:46 -0700

    Adding Task_3

---
+++ Task_3.py
@@ -0,0 +1,26 @@
+'''
+3.
+class Profile:
+    """
+    Create regular class taking 8 params on init - name, last_name, phone_number, address, email, birthday, age, sex
+    Override a printable string representation of Profile class and return: list of the params mentioned above
+    """
+'''
+class Profile:
+    def __init__(self, name, last_name, phone_number, address, email, birthday, age, sex):
+        self.name = name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.address = address
+        self.email = email
+        self.birthday = birthday
+        self.age = age
+        self.sex = sex
+        self.info = [self.name, self.last_name, self.phone_number, self.address, self.email, self.birthday, self.age, self.sex]
+
+    def __str__(self):
+        return f'All profile information: {self.info}'
+
+human = Profile('Ira', 'Semchuk', '098 618 3927', 'Lviv', 'iryna@gmail.com', '21.02.1999', '22', 'mate')
+
+print(human)
