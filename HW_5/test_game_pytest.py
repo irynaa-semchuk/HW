9c58a0b86c043f2c612bc8287a4cdf402e663c89
irynaa-semchuk <48826465+irynaa-semchuk@users.noreply.github.com>
2021-06-02 19:42:11 +0300

    Corrected the setup() method

--- HW_5/test_game_pytest.py
+++ HW_5/test_game_pytest.py
@@ -1,23 +1,18 @@
 import pytest
 from game import TicTacToe
 
-'''def setup_class(cls):
-    cls.game = TicTacToe()
-    cls.game.board[4] = "X"'''
-
-
-def setup():
-    global game
+@pytest.fixture(scope="module")
+def setup_():
     game = TicTacToe()
     game.board[4] = "X"
     return game
 
 
-def test_available_moves():
-    assert (4 not in setup().available_moves())
+def test_available_moves(setup_):
+    assert (4 not in setup_.available_moves())
+    
 
-
-def test_make_move():
-    assert not (setup().make_move(4, "X"))
-    assert (setup().make_move(5, "X"))
-    assert (game.board[5] != " ")
+def test_make_move(setup_):
+    assert not (setup_.make_move(4, "X"))
+    assert (setup_.make_move(5, "X"))
+    assert (setup_.board[5] != ' ')
