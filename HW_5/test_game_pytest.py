7eda7465a187460e76d63877fb226992a69d9428
irynaa-semchuk <48826465+irynaa-semchuk@users.noreply.github.com>
2021-06-07 23:03:17 +0300

    Update test_game_pytest.py

--- HW_5/test_game_pytest.py
+++ HW_5/test_game_pytest.py
@@ -1,18 +1,35 @@
 import pytest
 from game import TicTacToe
 
-@pytest.fixture(scope="module")
-def setup_():
+def setup_module():
+    global game
     game = TicTacToe()
     game.board[4] = "X"
-    return game
+    game.board[6] = "O"
 
 
-def test_available_moves(setup_):
-    assert (4 not in setup_.available_moves())
-    
+def test_available_moves():
+    assert (4 not in game.available_moves())
+    assert (6 not in game.available_moves())
+    assert (5 in game.available_moves())
+    assert (3 in game.available_moves())
 
-def test_make_move(setup_):
-    assert not (setup_.make_move(4, "X"))
-    assert (setup_.make_move(5, "X"))
-    assert (setup_.board[5] != ' ')
+
+def test_make_move():
+    assert not (game.make_move(4, "X"))
+    assert (game.make_move(5, "X"))
+    assert not (game.make_move(6, "O"))
+    assert (game.make_move(7, "O"))
+    assert (game.board[1] == " ")
+    assert (game.board[7] != " ")
+    assert (game.board[5] != " ")
+
+
+def test_make_move_negative_case():
+    with pytest.raises(IndexError):
+        assert game.make_move(10, "X")
+        assert game.make_move(9, "X")
+        assert game.make_move(6, "X")
+        assert game.make_move(4, "O")
+        assert game.make_move(2, "O")
+        assert game.make_move(1, "O")
