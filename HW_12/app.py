6f3779d75d8c614909843e78b49ce5c1f36ea61f
irynaa-semchuk <48826465+irynaa-semchuk@users.noreply.github.com>
2021-07-21 14:34:00 +0300

    Add method map

--- HW_12/app.py
+++ HW_12/app.py
@@ -3,23 +3,21 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+oper = ['sum', 'dif', 'mult', 'div']
+symbol = ['+', '-', '*', '/']
+FUNC = dict(map(lambda *args: args, oper, symbol))
+
 
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
-    return 'Hey, this is a calculator!\nEnter - "/ calc / x / y / operation" \nWhere the operator is: dif, sum, div or mul'
-    
-    
+    return 'Hey, this is a calculator!\tEnter - "/ calc / x / y / operation" \t Where the operator is: dif, sum, div or mul'
+
 
 @app.route('/calc/<int:x>/<int:y>/<string:oper>')
 def calc(x, y, oper):
-    if oper == 'dif':
-        return render_template('calc.html', x=x, y=y, result=x / y, symbol="/")
-    elif oper == 'sum':
-        return render_template('calc.html', x=x, y=y, result=x + y, symbol="+")
-    elif oper == 'div':
-        return render_template('calc.html', x=x, y=y, result=x - y, symbol="-")
-    elif oper == 'mul':
-        return render_template('calc.html', x=x, y=y, result=x * y, symbol="*")
+    func = FUNC.get(oper)
+    if func:
+        return render_template('calc.html', x=x, y=y, result=eval(f'{x} {func} {y}'), oper=str(func))
     else:
         return render_template('calc.html', result="Data was entered incorrectly")
 
